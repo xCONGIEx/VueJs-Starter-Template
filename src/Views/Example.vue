ec3aebc6849c2aa9dc0060ea3959fadb29bac564
Connor Gorr <xcongiex@gmail.com>
2021-08-05 20:58:29 -0700

    Example.vue added to show how a component is supposed to be usedwithin a view.

--- src/Views/Example.vue
+++ src/Views/Example.vue
@@ -1,17 +1,27 @@
 <template>
-    <p>This view is using the HelloWorld.vue file as a component within itself. You are essentially viewing a new web page right now vs what you saw on the homescreen.</p>
-  <HelloWorld />
+  <div id="content">
+    <p>
+      This view is using the HelloWorld.vue file as a component within itself.
+      You are essentially viewing a new web page right now vs what you saw on
+      the homescreen.
+    </p>
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
   name: "Example",
   components: {
-      HelloWorld
+    HelloWorld,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#content {
+  background-color: rgb(156, 156, 156);
+}
+</style>
