ee05d042b6997a4583de4730058888d57f33e4e8
Connor Gorr <xcongiex@gmail.com>
2021-08-05 17:00:04 -0700

    Added the vue router

---
+++ src/router/router.js
@@ -0,0 +1,13 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const history = createWebHistory();
+const router = createRouter({
+    history,
+    routes: [{
+        path: "/",
+        component: HelloWorld,
+    }, ],
+});
+
+export default router;
