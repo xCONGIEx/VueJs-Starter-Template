c67e9f94e6624132bf0198afe5199e875f72feba
Connor Gorr <xcongiex@gmail.com>
2021-08-05 20:54:12 -0700

    Example.vue added to show how a component is supposed to be usedwithin a view.

---
+++ src/Views/Example.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>This view is using the HelloWorld.vue file as a component within itself. You are essentially viewing a new web page right now vs what you saw on the homescreen.</p>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: "Example",
+  components: {
+      HelloWorld
+  },
+};
+</script>
+
+<style scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,19 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HelloWorld from "../components/HelloWorld.vue";
+import Example from "../Views/Example.vue";
 
 const history = createWebHistory();
 const router = createRouter({
     history,
     routes: [{
-        path: "/",
-        component: HelloWorld,
-    }, ],
+            path: "/",
+            component: HelloWorld,
+        },
+        {
+            path: "/example",
+            component: Example,
+        },
+    ],
 });
 
 export default router;
